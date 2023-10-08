1b1d9517bda4237ce20732939766011245805fa4
Abhishek Shukla <88367072+AVI-SHUKLA1110@users.noreply.github.com>
2023-10-08 23:54:56 +0530

    Update helloworld.sol

--- smart contract programmer/helloworld.sol
+++ smart contract programmer/helloworld.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract Introfunc {
+contract Intro {
     string  hi="HELLO WORLD!";
 }
 
-// it's okay to remove the public visibility modifier, and it won't throw an error. 
+// It's okay to remove the public visibility modifier, and it won't throw an error. 
 // The contract will still compile and deploy successfully.
 // Just keep in mind that without the public visibility, 
 // you'll need to add your own custom getter function 
