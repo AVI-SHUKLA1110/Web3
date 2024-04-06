14afd24a13cc44b60b932fb6c9670f5795850d62
Abhishek Shukla <88367072+AVI-SHUKLA1110@users.noreply.github.com>
2024-04-07 00:33:37 +0530

    Create constant.sol

---
+++ smart contract programmer/constant.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+contract Constants  {
+
+//it is a convention to write the constant variables in UpperCase 
+
+string public constant NAME = "Abhishek";
+uint public constant NUMBER = 182;
+address public constant ADD = 0xBd3762d227c776295DB9B6B4eb2EADE17BBe53AC;
+}
