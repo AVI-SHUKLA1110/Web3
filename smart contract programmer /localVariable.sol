c29e02776060020b69f677a9fbf65027e1273ed7
Abhishek Shukla <88367072+AVI-SHUKLA1110@users.noreply.github.com>
2024-04-07 00:40:29 +0530

    Create localVariable.sol

---
+++ smart contract programmer /localVariable.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+
+contract LocalVariables {
+    uint public a;
+    uint public b;     //state var
+    address public myadd;
+
+  function foo() external {
+        uint x = 123;
+        bool y = false;              // x, y = local variables
+
+        x += 456;
+        y = true;
+
+        //updating the state variables
+         a = 456;
+         b = 789;
+         myadd = address(1);
+    }
+}
+
+// the two local variables x and y executes only while the function is execurint 
+//after the function is executed we get that the state variables declared outside the function
+// at the top stores their value at the last section ("update the state variables")
